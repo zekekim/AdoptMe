22cd0dc678ee56c38b1c3c8b2de9c337369d89f1
Farhan1232001 <52764196+Farhan1232001@users.noreply.github.com>
2022-04-27 00:50:58 -0700

    Set the groundwork for the dog swiping page
    Note: Initial view controller has been changed

--- SwipingPageViewController.swift
+++ SwipingPageViewController.swift
@@ -13,12 +13,105 @@ class SwipingPageViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
+        ogCardPoint = card.center
+        
+        divisor = (view.frame.width / 2) / 0.61
     }
     
+    
+    
+    
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var dogImage: UIImageView!
     @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView! // Holds image of dog
+    //@IBOutlet weak var imageView: UIImageView! // Holds image of dog
+    @IBOutlet weak var card: UIView!
+    var ogCardPoint: CGPoint?
+    var divisor: CGFloat!
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        
+        // Allow card to move
+        card.center = CGPoint(x: ogCardPoint!.x + point.x, y: ogCardPoint!.y + point.y)
+        
+        // Rotate card left or right (Affine == to preserve parallel relatioship)
+        // 100/2 = 50/0.61 = 81.967
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter/divisor)
+        
+        // If card is being dragged right... else... left...
+        if xFromCenter > 0 {
+            thumbImageView.image = UIImage(named: "thumbsUp")
+            thumbImageView.tintColor = UIColor.green
+        } else {
+            thumbImageView.image = UIImage(named: "thumbsDown")
+            thumbImageView.tintColor = UIColor.red
+        }
+        
+        thumbImageView.alpha = abs(xFromCenter) / view.center.x
+        
+        // When finger lets go of card...
+        if sender.state == UIGestureRecognizer.State.ended {
+            
+            // If card close to edge of screen, move card off screen
+            if card.center.x < 75 {
+                // Move card to the left side
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+            } else if card.center.x > (view.frame.width - 75) {
+                // Move card to right side
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+            }
+            
+            
+            // Animate card back to center
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.ogCardPoint!
+                self.thumbImageView.alpha = 0
+                self.card.transform = CGAffineTransform.identity
+            })
+        }
+        
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        resetCard()
+    }
+    
+    func resetCard() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.card.center = self.ogCardPoint!
+            self.thumbImageView.alpha = 0
+            self.card.alpha = 1
+            // undos transformationn on card
+            self.card.transform = CGAffineTransform.identity
+        })
+    }
+    
+    // Function displays thumbs up on dogImage. Called when card is being swiped rightward
+    func displayThumbsUp(){
+        
+        let image = UIImage(systemName: "hand.thumbsup.fill")
+        image?.withTintColor(.black)
+        let imageView = UIImageView(image: image!)
+        
+        imageView.frame = CGRect(x: ogCardPoint!.x - 155, y: ogCardPoint!.y - 155, width: 222, height: 222)
+        dogImage.addSubview(imageView)
+                
+        //Imageview on Top of View
+        self.view.bringSubviewToFront(imageView)
+    }
+    
+    
     
     /*
     // Function queries dogs from PetFinder Api
